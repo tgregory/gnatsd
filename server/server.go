8376f394298acc5230932ba90e17aaa41e435017
Trifonov Grigory <trifonov.grigory@gmail.com>
2015-03-12 13:01:35 +0300

    Accidently broken HttpMonitoring
    Revert "Added custom listener support, this will let using custom transports in"
    
    This reverts commit 028026493b82484eb53875ccc9f24f8f5ed41680.

--- server/server.go
+++ server/server.go
@@ -32,18 +32,6 @@ type Info struct {
 	MaxPayload   int    `json:"max_payload"`
 }
 
-// Used for custom transport in embedded usage scenarios
-type ListenerCreator interface {
-	CreateListener(laddr string) (net.Listener, error)
-}
-
-// Default listener creator
-type TcpListenerCreator struct{}
-
-func (tlc TcpListenerCreator) CreateListener(laddr string) (net.Listener, error) {
-	return net.Listen("tcp", laddr)
-}
-
 // Server is our main struct.
 type Server struct {
 	mu       sync.Mutex
@@ -70,9 +58,6 @@ type Server struct {
 	routeInfo     Info
 	routeInfoJSON []byte
 	rcQuit        chan bool
-
-	// Used in custom transport setup
-	listenerCreator ListenerCreator
 }
 
 type stats struct {
@@ -84,11 +69,6 @@ type stats struct {
 
 // New will setup a new server struct after parsing the options.
 func New(opts *Options) *Server {
-	return NewCustom(opts, nil)
-}
-
-// Same as New but uses a custom listener/transport
-func NewCustom(opts *Options, listenerCreator ListenerCreator) *Server {
 	processOptions(opts)
 	info := Info{
 		ID:           genID(),
@@ -101,14 +81,13 @@ func NewCustom(opts *Options, listenerCreator ListenerCreator) *Server {
 	}
 
 	s := &Server{
-		info:            info,
-		sl:              sublist.New(),
-		opts:            opts,
-		debug:           opts.Debug,
-		trace:           opts.Trace,
-		done:            make(chan bool, 1),
-		start:           time.Now(),
-		listenerCreator: listenerCreator,
+		info:  info,
+		sl:    sublist.New(),
+		opts:  opts,
+		debug: opts.Debug,
+		trace: opts.Trace,
+		done:  make(chan bool, 1),
+		start: time.Now(),
 	}
 
 	s.mu.Lock()
@@ -128,7 +107,6 @@ func NewCustom(opts *Options, listenerCreator ListenerCreator) *Server {
 	s.handleSignals()
 
 	return s
-
 }
 
 // Sets the authentication method
@@ -291,11 +269,7 @@ func (s *Server) Shutdown() {
 func (s *Server) AcceptLoop() {
 	hp := fmt.Sprintf("%s:%d", s.opts.Host, s.opts.Port)
 	Noticef("Listening for client connections on %s", hp)
-	lc := s.listenerCreator
-	if nil == lc {
-		lc = TcpListenerCreator{}
-	}
-	l, e := lc.CreateListener(hp)
+	l, e := net.Listen("tcp", hp)
 	if e != nil {
 		Fatalf("Error listening on port: %s, %q", hp, e)
 		return
@@ -365,12 +339,7 @@ func (s *Server) StartHTTPMonitoring() {
 
 	hp := fmt.Sprintf("%s:%d", s.opts.Host, s.opts.HTTPPort)
 
-	lc := s.listenerCreator
-	if nil == lc {
-		lc = TcpListenerCreator{}
-	}
-
-	l, err := lc.CreateListener(hp)
+	l, err := net.Listen("tcp", hp)
 	if err != nil {
 		Fatalf("Can't listen to the monitor port: %v", err)
 	}
